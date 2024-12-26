cfb8ae3730e1d22befc79e54a4223850fc30e2a5
Normurodov Umarjon <userpeaseonly@gmail.com>
2024-12-26 05:45:16 +0500

    file upload api

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,19 @@
+import os
 from locust import HttpUser, task, between
 
-class FastAPIUser(HttpUser):
-    wait_time = between(1, 3)  # User will wait between 1 to 3 seconds between tasks
 
-    # Simulate a user making a GET request to the root endpoint
+
+class FileUploadUser(HttpUser):
+    wait_time = between(1, 3)  # Simulate user wait time between requests
+
     @task
-    def index(self):
-        self.client.get("/")  # Replace with your FastAPI root URL
+    def upload_file(self):
+        file_path = "uploads/50MB.zip"
+        # Ensure a test file exists
+        if not os.path.exists(file_path):
+            with open(file_path, "w") as f:
+                f.write("This is a test file for benchmarking FastAPI file upload speed.")
+
+        with open(file_path, "rb") as file:
+            files = {"file": (file_path, file, "text/plain")}
+            self.client.post("/upload/", files=files)
